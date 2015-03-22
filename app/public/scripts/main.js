bddfd093566c322f7318c0bff71288e7327cacbd
Pablo Valor <pablovalor89@gmail.com>
2015-03-21 22:31:59 -0300

    Added scrollTo animation into arrow header

--- app/public/scripts/main.js
+++ app/public/scripts/main.js
@@ -1,10 +1,22 @@
 $(document).on('ready', function() {
 	"use strict";
 		
-	var $header = $('#header');
+	var $header 		= $('#header'),
+		$arrowHeader 	= $('#arrow'),
+		headerHeight; 
 
 	//Set $header height related the viewport height
 	$header.height($(window).height());
+	headerHeight = $header.height();
+
+	//arrow header event
+	$arrowHeader.on('click', function(event) {
+		event.preventDefault();
+
+		$('html, body').animate({
+			scrollTop: headerHeight
+		}, 1500);
+	});
 
 	console.log('ready :)');	
 });
